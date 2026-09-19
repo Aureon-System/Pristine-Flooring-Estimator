7abac6e566ec72deee3d4036f6d1aa991f021468
Elcio Santos <elciousa001@gmail.com>
2026-09-19 17:06:08 -0400

    Add Stripe test checkout link for Pro

--- billing-config.js
+++ billing-config.js
@@ -10,6 +10,7 @@ window.PRISTINE_BILLING = {
       name: "Pro",
       monthly: 12.99,
       stripePriceId: "price_1UHVKiHOF48QQcVxwHeJBQpw",
+      checkoutUrl: "https://buy.stripe.com/test_00w3cvawF1MZ7nQh1p4c800",
       features: ["Cloud documents", "Email estimate/invoice", "Text customer", "Automated follow-up", "Client view", "Estimate acceptance", "Activity history"]
     }
   }
